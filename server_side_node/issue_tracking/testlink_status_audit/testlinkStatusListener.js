597c89e4ae094a3a080f635e4d45d3e284be3375
inpechman <inpechman@gmail.com>
2018-11-12 17:39:18 +0200

    rearranged folders

--- server_side_node/issue_tracking/testlink_status_audit/testlinkStatusListener.js
+++ server_side_node/issue_tracking/testlink_status_audit/testlinkStatusListener.js
@@ -4,6 +4,12 @@ const DEV_KEY = '20b497c0a4ae51e2869653bcca22727e';
 const {TL_HOST, TL_PORT, TL_API_PATH} = require('../../constants/constants');
 const COVERAGE = module.exports.COVERAGE = 'coverage';
 const EXECUTION = module.exports.EXECUTION = 'execution';
+const events = {};
+const TEST_FAILURE = events.TEST_FAILURE = 'test_failure';
+const TEST_PASSED = events.TEST_PASSED = 'test_passed';
+module.exports.events = events;
+// const TEST_FAILURE = module.exports.events.TEST_FAILURE = 'test_failure';
+// const TEST_PASSED = module.exports.events.TEST_PASSED = 'test_passed';
 
 class TestLinkAuditor extends EventEmitter {
 
@@ -23,10 +29,26 @@ class TestLinkAuditor extends EventEmitter {
             case COVERAGE:
                 break;
             case EXECUTION:
-                interval = setInterval((a, b, c) => {
+                interval = setInterval(async (a, b, c) => {
                     console.log(a, b, c);
+                    try {
+                        let testCases = await this._getTestCasesForTestPlan(identifier);
+                        console.log(testCases);
+                        for (let testCaseId of Object.keys(testCases)) {
+                            let testCase = testCases['' + testCaseId];
+                            if (testCase[0]['exec_status'] === "f") {
+                                this.emit(TEST_FAILURE, testCase);
+                            }
+                            if (testCase[0]['exec_status'] === "p") {
+                                this.emit(TEST_PASSED, testCase);
+                            }
+                        }
+                    } catch (e) {
+                        console.log(e)
+                    }
 
-                }, timeout,1234,6534,78979,'dfndhn');
+
+                }, timeout, 1234, 6534, 78979, 'dfndhn');
                 break;
         }
         return auditor;
@@ -37,10 +59,13 @@ class TestLinkAuditor extends EventEmitter {
     }
 
     async _getTestCasesForTestPlan(tPlanId) {
-        return await this.tlClient.sendRequest('getTestCasesForTestPlan', {testpalid: tPlanId})
+        return await this.tlClient.sendRequest('getTestCasesForTestPlan', {testplanid: tPlanId})
     }
 }
 
-let auditor = new TestLinkAuditor(TL_HOST, TL_API_PATH).startAudit(EXECUTION,'plan_1',2000);
-
-setTimeout(()=>auditor.stopAudit(),15000);
+let auditor = new TestLinkAuditor(TL_HOST, TL_API_PATH);
+let audit = auditor.startAudit(EXECUTION, '14', 5000);
+auditor.on(TEST_PASSED, args => {
+    console.log('test passed ', args);
+});
+setTimeout(() => audit.stopAudit(), 10000);
