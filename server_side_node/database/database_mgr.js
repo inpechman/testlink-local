ee412a7863094d50177d22989974472a8d58ef13
inpechman <inpechman@gmail.com>
2018-11-12 18:49:18 +0200

    started working on database

---
+++ server_side_node/database/database_mgr.js
@@ -0,0 +1,4 @@
+const mysql = require('mysql');
+
+const connectionDetails = {host:'localhost',user:'rtsuitetestlinksync',password:'Inpecman1', database:'rtsuitetestlinksync'};
+
--- server_side_node/issue_tracking/testlink_status_audit/testlinkStatusAuditor.js
+++ server_side_node/issue_tracking/testlink_status_audit/testlinkStatusAuditor.js
@@ -1,22 +1,19 @@
 const EventEmitter = require('events');
 const tlAPIClient = require('../../demo-tlapi-client/tlApiClient');
 const DEV_KEY = '20b497c0a4ae51e2869653bcca22727e';
-const {TL_HOST, TL_PORT, TL_API_PATH} = require('../../constants/constants');
+// const {TL_HOST, TL_PORT, TL_API_PATH} = require('../../constants/constants');
 const COVERAGE = module.exports.COVERAGE = 'coverage';
 const EXECUTION = module.exports.EXECUTION = 'execution';
 const events = {};
 const TEST_FAILURE = events.TEST_FAILURE = 'test_failure';
 const TEST_PASSED = events.TEST_PASSED = 'test_passed';
 module.exports.events = events;
-// const TEST_FAILURE = module.exports.events.TEST_FAILURE = 'test_failure';
-// const TEST_PASSED = module.exports.events.TEST_PASSED = 'test_passed';
+
 
 class TestLinkAuditor extends EventEmitter {
 
     constructor(tlHost, tlAPIPath) {
         super();
-        this.tlHost = tlHost;
-        this.tlAPIPath = tlAPIPath;
         this.tlClient = tlAPIClient.createTLClient(tlHost, 80, tlAPIPath);
         this.tlClient.setDevKey(DEV_KEY)
     }
@@ -29,8 +26,7 @@ class TestLinkAuditor extends EventEmitter {
             case COVERAGE:
                 break;
             case EXECUTION:
-                interval = setInterval(async (a, b, c) => {
-                    console.log(a, b, c);
+                interval = setInterval(async () => {
                     try {
                         let testCases = await this._getTestCasesForTestPlan(identifier);
                         console.log(testCases);
@@ -46,9 +42,7 @@ class TestLinkAuditor extends EventEmitter {
                     } catch (e) {
                         console.log(e)
                     }
-
-
-                }, timeout, 1234, 6534, 78979, 'dfndhn');
+                }, timeout);
                 break;
         }
         return auditor;
@@ -59,9 +53,10 @@ class TestLinkAuditor extends EventEmitter {
     }
 }
 
-let auditor = new TestLinkAuditor(TL_HOST, TL_API_PATH);
-let audit = auditor.startAudit(EXECUTION, '14', 5000);
-auditor.on(TEST_PASSED, args => {
-    console.log('test passed ', args);
-});
-setTimeout(() => audit.stopAudit(), 10000);
+module.exports.default = TestLinkAuditor;
+// let auditor = new TestLinkAuditor(TL_HOST, TL_API_PATH);
+// let audit = auditor.startAudit(EXECUTION, '14', 5000);
+// auditor.on(TEST_PASSED, args => {
+//     console.log('test passed ', args);
+// });
+// setTimeout(() => audit.stopAudit(), 10000);
