f9597bac0601d3144a9e00163e7d550214293607
inpechman <inpechman@gmail.com>
2018-11-14 17:27:20 +0200

    started working on database

---
+++ server_side_node/database/init_tables.sql
@@ -0,0 +1,57 @@
+CREATE TABLE projects
+(
+  id            int PRIMARY KEY,
+  tl_project_id int,
+  name          text
+);
+CREATE TABLE req_specs
+(
+  id             int PRIMARY KEY,
+  tl_req_spec_id int,
+  parent_id      int,
+  name           text,
+  scope          text
+);
+CREATE TABLE requirements
+(
+  id                INT PRIMARY KEY,
+  tl_requirement_id INT,
+  name              TEXT,
+  scope             TEXT,
+  req_spec_id       INT
+);
+CREATE TABLE test_plans
+(
+  id          int PRIMARY KEY,
+  tl_tplan_id int,
+  project_id  int,
+  name        text
+);
+CREATE TABLE builds
+(
+  id           int PRIMARY KEY,
+  tl_build_id  int,
+  test_plan_id int,
+  name         text
+);
+CREATE TABLE bugs
+(
+  id                   int PRIMARY KEY,
+  title                text,
+  details              text,
+  test_case_id         int,
+  tester_id            int,
+  execution_time_stamp text,
+  execution_id         int,
+  execution_status     int,
+  report_count         int
+);
+CREATE TABLE bugs_to_be_tested
+(
+  id                        int PRIMARY KEY,
+  test_case_id              int,
+  execution_status          text,
+  last_execution_time_stamp text,
+  test_plan_id              int,
+  report_count              int
+)
