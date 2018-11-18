a90854b19513c86d4a5be82ad870ab86ed3c4009
sf <shlomif83@gmail.com>
2018-11-18 12:49:26 +0200

    wirte functions to add test cases to test plan by case Id

--- server_side_node/issue_tracking/to_be_tested/to_be_tested.js
+++ server_side_node/issue_tracking/to_be_tested/to_be_tested.js
@@ -11,36 +11,46 @@ async function getTestPlanId(projectName, planName) {
 
 async function getTestCaseId(projectName, testCaseName) {
     let testCase = await client.sendRequest('getTestCaseIDByName', { testprojectname: projectName, testcasename: testCaseName });
+    console.log(testCase);
     let testCaseID = testCase[0].id
     return testCaseID;
 }
+// getTestCaseId('TRB', 'my test case on my test suit');
 
-async function getTestCaseExtrenalIdOrTestCaseVersion(projectName, testCaseName, extrenalID_or_version) {
-    let testCaseID = await getTestCaseId(projectName, testCaseName);
-    let testCase = await client.sendRequest('getTestCase', { testcasename: projectName, testcasename: testCaseName, testcaseid: testCaseID })
-    let extrenalID = testCase[0].tc_external_id;
-    let version = testCase[0].version;
-    if (extrenalID_or_version == "extrenalID") {
-        return extrenalID;
-    }
-    if (extrenalID_or_version == "version") {
-        return version
-    }
+
+// async function getTestCaseExtrenalIdOrTestCaseVersion(projectName, testCaseName, extrenalID_or_version) {
+//     let testCaseID = await getTestCaseId(projectName, testCaseName);
+//     let testCase = await client.sendRequest('getTestCase', { testcasename: testCaseName, testcaseid: testCaseID })
+//     let extrenalID = testCase[0].tc_external_id;
+//     let version = testCase[0].version;
+//     if (extrenalID_or_version == "extrenalID") {
+//         return extrenalID;
+//     }
+//     if (extrenalID_or_version == "version") {
+//         return version
+//     }
+// }
+
+async function getTestCase(projectName, testCaseID) {
+    let testCase = await client.sendRequest('getTestCase', { testprojectname: projectName, testcaseid: testCaseID })
+    // console.log(testCase[0]);
+    return testCase[0];
 }
 
 
-
-async function addTestCaseToTestPlan(projectName, planName, testCaseName) {
+async function addTestCaseToTestPlan(projectName, planName, testCaseID_Arr) {
     try {
         let projectId = await getProjectIdByName.getProjectIdByName(projectName);
         let projectPrefix = await getProjectIdByName.getProjectIdByName(projectName, true)
         let planId = await getTestPlanId(projectName, planName);
-        let extrenalID = await getTestCaseExtrenalIdOrTestCaseVersion(projectName, testCaseName, "extrenalID");
-        let version = await getTestCaseExtrenalIdOrTestCaseVersion(projectName, testCaseName, "version");
-        let doTestCaseToTestPlan = await client.sendRequest('addTestCaseToTestPlan', {
-            testprojectid: projectId, testplanid: planId,
-            testcaseexternalid: projectPrefix + "-" + extrenalID, version: parseInt(version)
-        });
+        for (let i = 0; i < testCaseID_Arr.length; i++) {
+            let testCase = await getTestCase(projectName, testCaseID_Arr[i]);
+            let version = await testCase.version;
+            let doTestCaseToTestPlan = await client.sendRequest('addTestCaseToTestPlan', {
+                testprojectid: projectId, testplanid: planId,
+            /*testcaseexternalid: projectPrefix + "-" + extrenalID,*/testcaseid: testCaseID_Arr[i], version: parseInt(version)
+            });
+        }
     } catch (error) {
         console.log(error);
 
@@ -49,7 +59,6 @@ async function addTestCaseToTestPlan(projectName, planName, testCaseName) {
 }
 
 
-// addTestCaseToTestPlan('TRB', '12/11/2018', 'my test case on my test suit')
 
 async function createDate() {
     let fullDate = 'RETEST- ' + new Date(new Date().toString().split('GMT')[0] + ' UTC').toISOString().split('.')[0].replace('T', ' ');
@@ -61,12 +70,15 @@ async function createBuild(projectName, planName) {
     await client.sendRequest('createBuild', { testprojectname: projectName, testplanid: testPlanId, buildname: "build for retest plan" });
 }
 
-async function createTestPlan(projectName, testCaseName) {
+
+
+async function create_Test_Plan_And_Add_TC_to_TP(projectName, testCaseID) {
     let planName = await createDate();
     try {
         await client.sendRequest('createTestPlan', { testplanname: planName, testprojectname: projectName });
         await createBuild(projectName, planName);
-        await addTestCaseToTestPlan(projectName, planName, testCaseName)
+        await addTestCaseToTestPlan(projectName, planName, testCaseID)
+
     }
     catch (error) {
         console.log(error);
@@ -74,5 +86,12 @@ async function createTestPlan(projectName, testCaseName) {
     }
 }
 
-// createTestPlan("TRB",'my test case on my test suit')
+
+
+
+
+create_Test_Plan_And_Add_TC_to_TP("TRB", ['564','572']);
+
+
+
 
