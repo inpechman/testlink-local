b02e2ef6c79fdd75a12fc4c2aa40b40d1dec31c0
inpechman <inpechman@gmail.com>
2018-11-19 17:14:20 +0200

    issue tracking

--- server_side_node/issue_tracking/trello_client/rt_trello_client.js
+++ server_side_node/issue_tracking/trello_client/rt_trello_client.js
@@ -1,9 +1,11 @@
 module.exports.addIssue = async function (id, title, details) {
+    console.log('addIssue args: ',arguments);
     // TODO: replace with real trello module communication
     return {web_url:'http://dummyurltobug.bugs/demo/demo'}
 };
 
 module.exports.addNote = async function (id, details) {
+    console.log('addNote args: ',arguments);
     // TODO: replace with real trello module communication
     return {status_ok:1}
 };
